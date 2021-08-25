2aea1c71d32fd0f68acddcddd7fd056d39740e27
Nitsan Litov <nitsanlitov@gmail.com>
2021-08-25 15:29:07 +0300

    prettify file

--- ChessClient/src/server/server.js
+++ ChessClient/src/server/server.js
@@ -14,7 +14,7 @@ const cookieAgeMs = 1000 * 10
 //session middleware
 app.use(sessions({
     secret: "a9s8dasdn89auyv9n3y5v3y5bvh8945u6j",
-    saveUninitialized:true,
+    saveUninitialized: true,
     cookie: { maxAge: cookieAgeMs },
     resave: false
 }));
@@ -24,73 +24,51 @@ app.use(cookieParser());
 sessionClients = {}
 
 app.get('/api/connect', (req, res) => {
-	const session = req.session
-	let userId = session.userId
+    const session = req.session
+    let userId = session.userId
 
-	if (!userId) {
-		userId = uuidv4()
-		session.userId = userId
-	}
-	if (!(session.userId in sessionClients) || !sessionClients[session.userId]) {
-		var readCallback = (data) => { console.log(data) }
-		client = communication.connect(readCallback)
-		
-	    let clientEndInterval = setInterval(() => { client.destroy() }, cookieAgeMs);
+    if (!userId) {
+        userId = uuidv4()
+        session.userId = userId
+    }
+    if (!(session.userId in sessionClients) || !sessionClients[session.userId]) {
+        var readCallback = (data) => { console.log(data) }
+        client = communication.connect(readCallback)
 
-	    client.on('close', function() {
-	        console.log('Connection closed');
-	        sessionClients[session.userId] = undefined
-	        clearInterval(clientEndInterval)
-	    });
-		sessionClients[session.userId] = client
-	}
-	res.status(200)
-	res.end()
+        let clientEndInterval = setInterval(() => { client.destroy() }, cookieAgeMs);
+
+        client.on('close', function() {
+            console.log('Connection closed');
+            sessionClients[session.userId] = undefined
+            clearInterval(clientEndInterval)
+        });
+        sessionClients[session.userId] = client
+    }
+    res.status(200)
+    res.end()
 })
 
 app.get('/api/session', (req, res) => {
-	const session = req.session
+    const session = req.session
 
-	console.log(session.userId)
-	res.status(200)
-	res.end()
+    console.log(session.userId)
+    res.status(200)
+    res.end()
 })
 
 app.get('/api/send', (req, res) => {
-	const session = req.session
-	const client = sessionClients[session.userId]
+    const session = req.session
+    const client = sessionClients[session.userId]
 
-	if (!client) {
-		res.status(400)
-		res.end()
-		return
-	}
+    if (!client) {
+        res.status(400)
+        res.end()
+        return
+    }
 
-	communication.sendJson(client, {"message": "OKKKKKK"})
-	res.status(200)
-	res.end()
+    communication.sendJson(client, { "message": "OKKKKKK" })
+    res.status(200)
+    res.end()
 })
-
-// app.get('/api/connect', (req, res) => {
-//     const malId = req.query["mal_id"]
-//     const watchedEpisodes = Number(req.query["watched_episodes"])
-//     const setCompleted = Boolean(req.query["set_completed"])
-//     const username = req.query["username"]
-
-//     if (!malId) {
-//         res.end(`mal_id is mandatory`)
-//         return
-//     }
-//     if (!watchedEpisodes) {
-//         res.end(`watched_episodes is mandatory and supposed to be a number`)
-//         return
-//     }
-//     if (!username) {
-//         res.end(`username is mandatory`)
-//         return
-//     }
-
-//     handleMalApiResult(mal.updateWatchedEpisodes(username, malId, watchedEpisodes, setCompleted), res)
-// })
 
 app.listen(port, () => {})
