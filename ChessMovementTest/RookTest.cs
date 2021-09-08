c85c40cbbd5158a2be71fa49a0212449bcb1e7e6
Nitsan Litov <nitsanlitov@gmail.com>
2021-09-08 19:13:02 +0300

    Completed rook movement tests

--- ChessMovementTest/RookTest.cs
+++ ChessMovementTest/RookTest.cs
@@ -14,38 +14,55 @@ namespace ChessMovementTest
         public void TestWhiteRook()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
-            TestHelper.PrintAll(board);
 
             List<ChessPiece> firstPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.FirstPlayer)];
             List<ChessPiece> secondPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.SecondPlayer)];
-            
-            firstPieces[(int)FirstPiecesNumber.RightRook].ForceMove(('d', 4));
+
+            ChessPiece rook = firstPieces[(int)FirstPiecesNumber.RightRook];
+            string movementOptions = "d5 d6 d7 d3 c4 b4 a4 e4 f4 ";
+
             TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, "");
+            
+            rook.ForceMove(('d', 4));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, movementOptions + "g4 h4");
             
             firstPieces[(int)FirstPiecesNumber.RightBishop].ForceMove(('g', 4));
             TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, movementOptions);
             
             secondPieces[(int)SecondPiecesNumber.RightBishop].ForceMove(('g', 4));
             TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, movementOptions + "g4");
         }
         
         [TestMethod]
         public void TestBlackRook()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
-            TestHelper.PrintAll(board);
 
             List<ChessPiece> firstPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.FirstPlayer)];
             List<ChessPiece> secondPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.SecondPlayer)];
+
+            ChessPiece rook = secondPieces[(int)SecondPiecesNumber.RightRook];
+            string movementOptions = "d5 d6 d2 d3 c4 b4 a4 e4 f4 ";
             
-            secondPieces[(int)SecondPiecesNumber.RightRook].ForceMove(('d', 4));
+
             TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, "");
+            
+            rook.ForceMove(('d', 4));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, movementOptions + "g4 h4");
             
             firstPieces[(int)FirstPiecesNumber.RightBishop].ForceMove(('g', 4));
             TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, movementOptions + "g4");
             
             secondPieces[(int)SecondPiecesNumber.RightBishop].ForceMove(('g', 4));
             TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(rook, movementOptions);
         }
     }
 }
