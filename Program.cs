1944c4feb380257955eb9cf319c2d18845c60d51
Nitsan Litov <nitsanlitov@gmail.com>
2021-09-08 19:45:18 +0300

    removed useless code

--- Program.cs
+++ Program.cs
@@ -8,41 +8,6 @@ namespace SuperChess
     {
         static void Main(string[] args)
         {
-            // GeneralTests.ShowBasicBoard();
-            // PawnTests.CheckWhiteMovement();
-            // PawnTests.CheckBlackMovement();
-            // RookTests.CheckWhiteMovement();
-            // RookTests.CheckBlackMovement();
-            // TakingTests.ValidateDeadPieceCantMove();
-            // QueenTests.CheckWhiteMovement();
-            // QueenTests.CheckBlackMovement();
-
-
-
-
-            // create board kill piece function that removes from list and places null instead. when moving piece if not null throw exception.
-
-
-
-
-            // king basic movement
-            // bishop basic movement
-            // knight basic movement
-
-            // castling
-                // if threat in between
-                // if live threat
-                // if not first move for each
-                // if pieces are in the middle
-
-            // en passent
-            // pawn becomes queen
-
-            // if in check basic movements arent allowed
-            // move that will allow check isnt allowed
-
-            // checkmate shows no movement options
-            // check for mate
         }
     }
 }
