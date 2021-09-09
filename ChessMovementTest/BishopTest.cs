322fcca6cd9c6a524b923b825433cfdbe5bd3281
EranDaniel98 <41913344+EranDaniel98@users.noreply.github.com>
2021-09-09 17:01:31 +0300

    Finished testing movements for king, knight and bishop

--- ChessMovementTest/BishopTest.cs
+++ ChessMovementTest/BishopTest.cs
@@ -39,12 +39,21 @@ namespace ChessMovementTest
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(wLeftBishop, "b2 a3 d2 e3 f4 g5 h6");
             TestHelper.ValidateMovementResults(wRightBishop, "g2 h3");
-            
+
             board.Move(('e', 2), ('e', 4));
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(wLeftBishop, "b2 a3 d2 e3 f4 g5 h6");
             TestHelper.ValidateMovementResults(wRightBishop, "g2 h3 e2 d3 c4 b5 a6");
-            throw new Exception();
+
+            board.Move(('c', 1), ('g', 5));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(wLeftBishop, "c1 d2 e3 f4 h4 h6 f6 e7");
+            TestHelper.ValidateMovementResults(wRightBishop, "g2 h3 e2 d3 c4 b5 a6");
+
+            board.Move(('f', 1), ('b', 5));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(wLeftBishop, "c1 d2 e3 f4 h4 h6 f6 e7");
+            TestHelper.ValidateMovementResults(wRightBishop, "a4 a6 c4 d3 e2 f1 c6 d7");
         }
 
         [TestMethod]
@@ -53,32 +62,44 @@ namespace ChessMovementTest
             Board board = TestHelper.CreateTwoPlayerBoard();
 
             List<ChessPiece> secondPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.SecondPlayer)];
-            ChessPiece wLeftBishop = secondPieces[(int)SecondPiecesNumber.King];
-            ChessPiece wRightBishop = secondPieces[(int)SecondPiecesNumber.King];
+            ChessPiece bLeftBishop = secondPieces[(int)SecondPiecesNumber.LeftBishop];
+            ChessPiece bRightBishop = secondPieces[(int)SecondPiecesNumber.RightBishop];
 
-            TestHelper.ValidateMovementResults(wLeftBishop, "");
-            TestHelper.ValidateMovementResults(wRightBishop, "");
+            TestHelper.ValidateMovementResults(bLeftBishop, "");
+            TestHelper.ValidateMovementResults(bRightBishop, "");
+            TestHelper.PrintAll(board);
 
-            //black king
-            board.Move(('d', 7), ('d', 6));
+            //black bishop
+            board.Move(('b', 7), ('b', 6));
             TestHelper.PrintAll(board);
-            TestHelper.ValidateMovementResults(wLeftBishop, "d7");
-            TestHelper.ValidateMovementResults(wRightBishop, "d7");
+            TestHelper.ValidateMovementResults(bLeftBishop, "");
+            TestHelper.ValidateMovementResults(bRightBishop, "b7 a6");
 
-            board.Move(('e', 8), ('d', 7));
+            board.Move(('g', 7), ('g', 6));
             TestHelper.PrintAll(board);
-            TestHelper.ValidateMovementResults(wLeftBishop, "c6 e6 e8");
-            TestHelper.ValidateMovementResults(wRightBishop, "c6 e6 e8");
+            TestHelper.ValidateMovementResults(bLeftBishop, "g7 h6");
+            TestHelper.ValidateMovementResults(bRightBishop, "b7 a6");
 
-            board.Move(('d', 7), ('c', 6));
+            board.Move(('d', 7), ('d', 5));
             TestHelper.PrintAll(board);
-            TestHelper.ValidateMovementResults(wLeftBishop, "b6 d7 b5 c5 d5");
-            TestHelper.ValidateMovementResults(wRightBishop, "b6 d7 b5 c5 d5");
-            
-            board.Move(('c', 6), ('b', 5));
+            TestHelper.ValidateMovementResults(bLeftBishop, "g7 h6");
+            TestHelper.ValidateMovementResults(bRightBishop, "b7 a6 d7 e6 f5 g4 h3");
+
+            board.Move(('e', 7), ('e', 5));
             TestHelper.PrintAll(board);
-            TestHelper.ValidateMovementResults(wLeftBishop, "a6 b6 c6 a5 c5 a4 b4 c4");
-            TestHelper.ValidateMovementResults(wRightBishop, "a6 b6 c6 a5 c5 a4 b4 c4");
+            TestHelper.ValidateMovementResults(bLeftBishop, "g7 h6 e7 d6 c5 b4 a3");
+            TestHelper.ValidateMovementResults(bRightBishop, "b7 a6 d7 e6 f5 g4 h3");
+
+            board.Move(('c', 8), ('g', 4));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(bLeftBishop, "g7 h6 e7 d6 c5 b4 a3");
+            TestHelper.ValidateMovementResults(bRightBishop, "c8 d7 e6 f5 h5 h3 f3 e2");
+
+            board.Move(('f', 8), ('b', 4));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(bLeftBishop, "a5 a3 c5 d6 e7 f8 c3 d2");
+            TestHelper.ValidateMovementResults(bRightBishop, "c8 d7 e6 f5 h5 h3 f3 e2");
+            throw new Exception();
         }
     }
 }
