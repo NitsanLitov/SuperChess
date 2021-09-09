94d56bde13981b0316fa3b756aaf9d318d2ba720
Nitsan Litov <nitsanlitov@gmail.com>
2021-09-09 17:22:54 +0300

    Added taking tests

---
+++ ChessTakingTest/CheckTest.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessTakingTest
+{
+    [TestClass]
+    public class CheckTest
+    {
+    }
+}
---
+++ ChessTakingTest/TakingTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessTakingTest
+{
+    [TestClass]
+    public class TakingTest
+    {
+        [TestMethod]
+        public void TestDeadPieceCantMove()
+        {
+        }
+    }
+}
