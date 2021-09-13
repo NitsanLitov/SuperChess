389bf737ee0cd13212d6b00d9fd36a5ee8672f03
EranDaniel98 <41913344+EranDaniel98@users.noreply.github.com>
2021-09-13 21:28:45 +0300

    Changed method to dispose pieces, added a test for both king and the space between threatened (together), added a threat on king directly alone, checked the movement options for all the pieces i've used

--- ChessTests/CastlingTest.cs
+++ ChessTests/CastlingTest.cs
@@ -11,7 +11,7 @@ namespace ChessTests
     [TestClass]
     public class CastlingTest
     {
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestWhiteCastling()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
@@ -41,7 +41,7 @@ namespace ChessTests
             TestHelper.ValidateMovementResults(wRightRook, "e1");
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestBlackCastling()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
@@ -74,7 +74,7 @@ namespace ChessTests
             TestHelper.ValidateMovementResults(bRightRook, "e8");
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestBlackThreatCastlingAfter()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
@@ -85,42 +85,34 @@ namespace ChessTests
             ChessPiece wKing = firstPieces[(int)FirstPiecesNumber.King];
             ChessPiece wLeftRook = firstPieces[(int)FirstPiecesNumber.LeftRook];
 
-            ChessPiece wQueen = firstPieces[(int)FirstPiecesNumber.Queen];
-            ChessPiece wLeftBishop = firstPieces[(int)FirstPiecesNumber.LeftBishop];
-            ChessPiece wLeftKnight = firstPieces[(int)FirstPiecesNumber.LeftKnight];
-            ChessPiece bPawn2 = secondPieces[(int)SecondPiecesNumber.Pawn2];
             ChessPiece bLeftBishop = secondPieces[(int)SecondPiecesNumber.LeftBishop];
 
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(wKing, "");
-
-            wQueen.Dispose();
-            wLeftBishop.Dispose();
-            wLeftKnight.Dispose();
-            bPawn2.Dispose();
+            firstPieces[(int)FirstPiecesNumber.Queen].Dispose();
+            firstPieces[(int)FirstPiecesNumber.LeftBishop].Dispose();
+            firstPieces[(int)FirstPiecesNumber.LeftKnight].Dispose();
+            secondPieces[(int)SecondPiecesNumber.Pawn1].Dispose();
 
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(wKing, "d1 c1");
             TestHelper.ValidateMovementResults(bLeftBishop, "g7 h6");
 
             //threat the space between the king and the rook
-            board.Move(('f', 8), ('g', 7));
+            board.Move(('f', 8), ('h', 6));
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(wKing, "d1 c1");
             TestHelper.ValidateMovementResults(wLeftRook, "b1 c1 d1");
-
-            board.Move(('g', 7), ('h', 6));
-            TestHelper.PrintAll(board);
-            TestHelper.ValidateMovementResults(wKing, "d1 c1");
-            TestHelper.ValidateMovementResults(wLeftRook, "b1 c1 d1");
+            TestHelper.ValidateMovementResults(bLeftBishop, "f8 g7 g5 f4 e3 d2");
 
             board.Move(('d', 2), ('d', 4));
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(wKing, "d1");
             TestHelper.ValidateMovementResults(wLeftRook, "b1 c1 d1");
+            TestHelper.ValidateMovementResults(bLeftBishop, "f8 g7 g5 f4 e3 d2 c1");
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestWhiteThreatCastlingInBetween()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
@@ -130,42 +122,53 @@ namespace ChessTests
 
             ChessPiece bKing = secondPieces[(int)SecondPiecesNumber.King];
             ChessPiece bLeftRook = secondPieces[(int)SecondPiecesNumber.RightRook];
+            ChessPiece bPawn6 = secondPieces[(int)SecondPiecesNumber.Pawn6];
 
-            ChessPiece bQueen = secondPieces[(int)SecondPiecesNumber.Queen];
-            ChessPiece bLeftBishop = secondPieces[(int)SecondPiecesNumber.RightBishop];
-            ChessPiece bLeftKnight = secondPieces[(int)SecondPiecesNumber.RightKnight];
-            ChessPiece wPawn3 = firstPieces[(int)FirstPiecesNumber.Pawn3];
+            ChessPiece wQueen = firstPieces[(int)FirstPiecesNumber.Queen];
 
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(bKing, "");
             TestHelper.ValidateMovementResults(bLeftRook, "");
+            TestHelper.ValidateMovementResults(wQueen, "");
 
-            bQueen.Dispose();
-            bLeftBishop.Dispose();
-            bLeftKnight.Dispose();
-            wPawn3.Dispose();
+            secondPieces[(int)SecondPiecesNumber.Queen].Dispose();
+            secondPieces[(int)SecondPiecesNumber.RightBishop].Dispose();
+            secondPieces[(int)SecondPiecesNumber.RightKnight].Dispose();
+            firstPieces[(int)FirstPiecesNumber.Pawn3].Dispose();
 
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(bKing, "d8 c8");
             TestHelper.ValidateMovementResults(bLeftRook, "b8 c8 d8");
+            TestHelper.ValidateMovementResults(wQueen, "c2 b3 a4");
 
             board.Move(('d', 1), ('b', 3));
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(bKing, "d8 c8");
             TestHelper.ValidateMovementResults(bLeftRook, "b8 c8 d8");
+            TestHelper.ValidateMovementResults(wQueen, "a3 a4 b4 b5 b6 b7 c4 d5 e6 f7 c3 d3 e3 f3 g3 h3 c2 d1");
 
             board.Move(('b', 7), ('b', 6));
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(bKing, "d8 c8");
             TestHelper.ValidateMovementResults(bLeftRook, "b8 c8 d8");
+            TestHelper.ValidateMovementResults(wQueen, "a3 a4 b4 b5 b6 c4 d5 e6 f7 c3 d3 e3 f3 g3 h3 c2 d1");
 
             board.Move(('b', 3), ('b', 6));
             TestHelper.PrintAll(board);
             TestHelper.ValidateMovementResults(bKing, "d8 c8");
             TestHelper.ValidateMovementResults(bLeftRook, "b8 c8 d8");
+            TestHelper.ValidateMovementResults(wQueen, "a5 a6 a7 b7 b8 c7 c6 d6 e6 f6 g6 h6 c5 d4 e3 b5 b4 b3");
+            TestHelper.ValidateMovementResults(bPawn6, "c6 c5 b6");
+
+            board.Move(('c', 7), ('c', 6));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(bKing, "");
+            TestHelper.ValidateMovementResults(bPawn6, "c5");
+            TestHelper.ValidateMovementResults(bLeftRook, "b8 c8 d8");
+            TestHelper.ValidateMovementResults(wQueen, "a5 a6 a7 b7 b8 c7 d8 c6 c5 d4 e3 b5 b4 b3");
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestWhiteCastlingPathIsNotEmpty()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
@@ -176,6 +179,10 @@ namespace ChessTests
             ChessPiece wKing = firstPieces[(int)FirstPiecesNumber.King];
             ChessPiece wLeftRook = firstPieces[(int)FirstPiecesNumber.LeftRook];
 
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(wKing, "");
+            TestHelper.ValidateMovementResults(wLeftRook, "");
+
             firstPieces[(int)FirstPiecesNumber.Queen].Dispose();
             firstPieces[(int)FirstPiecesNumber.LeftBishop].Dispose();
 
@@ -184,7 +191,44 @@ namespace ChessTests
             TestHelper.ValidateMovementResults(wLeftRook, "");
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
+        public void TestCastlingWhiteThreatBlackKing()
+        {
+            Board board = TestHelper.CreateTwoPlayerBoard();
+
+            List<ChessPiece> firstPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.FirstPlayer)];
+            List<ChessPiece> secondPieces = board.chessPiecesByColor[PlayerColor.GetColor(PlayerNumber.SecondPlayer)];
+
+            ChessPiece bKing = secondPieces[(int)SecondPiecesNumber.King];
+            ChessPiece bRightRook = secondPieces[(int)SecondPiecesNumber.LeftRook];
+            ChessPiece bPawn5 = secondPieces[(int)SecondPiecesNumber.Pawn5];
+            ChessPiece wQueen = firstPieces[(int)FirstPiecesNumber.Queen];
+
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(bKing, "");
+            TestHelper.ValidateMovementResults(bRightRook, "");
+            TestHelper.ValidateMovementResults(wQueen, "");
+            TestHelper.ValidateMovementResults(bPawn5, "d6 d5");
+
+            firstPieces[(int)FirstPiecesNumber.Pawn3].Dispose();
+            secondPieces[(int)SecondPiecesNumber.LeftKnight].Dispose();
+            secondPieces[(int)SecondPiecesNumber.LeftBishop].Dispose();
+
+            board.Move(('d', 7), ('d', 6));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(bPawn5, "d5");
+            TestHelper.ValidateMovementResults(bKing, "d7 f8 g8");
+            TestHelper.ValidateMovementResults(bRightRook, "g8 f8");
+            TestHelper.ValidateMovementResults(wQueen, "c2 b3 a4");
+
+            board.Move(('d', 1), ('a', 4));
+            TestHelper.PrintAll(board);
+            TestHelper.ValidateMovementResults(bKing, "f8");
+            TestHelper.ValidateMovementResults(bRightRook, "");
+            TestHelper.ValidateMovementResults(wQueen, "a3 a5 a6 a7 b3 c2 d1 b4 c4 d4 e4 f4 g4 h4 b5 c6 d7 e8");
+        }
+
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestWhiteCastlingKingNotFirstMove()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
@@ -218,7 +262,7 @@ namespace ChessTests
             TestHelper.ValidateMovementResults(wLeftRook, "b1 c1 d1");
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Movement"), TestCategory("Castling")]
         public void TestBlackCastlingRookNotFirstMove()
         {
             Board board = TestHelper.CreateTwoPlayerBoard();
